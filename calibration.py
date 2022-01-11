53daddb539b85d7368010d399583f384a56b383b
isaak23 <espio23@hotmail.com>
2022-01-11 04:02:56 +0100

    calibration program added

---
+++ calibration.py
@@ -0,0 +1,98 @@
+import statsmodels.api as sm
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+
+import io
+
+from datetime import datetime
+import struct
+from time import sleep
+import PySimpleGUI as sg
+import os.path
+from PySimpleGUI.PySimpleGUI import T, Multiline, popup
+from serial import *
+import serial.tools.list_ports
+import glob
+import numpy as np
+from struct import *
+from struct import Struct
+from tkinter.font import Font
+
+from collections import namedtuple
+import time 
+
+
+# VARS CONSTS:
+_VARS = {'window': False}
+
+
+
+
+def draw_figure(canvas, figure):
+    figure_canvas_agg = FigureCanvasTkAgg(figure, canvas)
+    figure_canvas_agg.draw()
+    figure_canvas_agg.get_tk_widget().pack(side='top', fill='both', expand=1)
+    return figure_canvas_agg
+
+#nel main c'è il layout e il ciclo infinito
+def main():
+    
+    #AppFont = 'Any 16'
+    #font = "Arial 13"
+    font_family, font_size = font = ('Courier New', 10)
+    sg.set_options(font=font)
+
+    layout =    [
+
+                [sg.Canvas(key='figCanvas')],
+                [sg.T('File'), sg.In(key = "flash-data-filepath", readonly=True),sg.Button('Open File',key='-OPEN-'),sg.Button('Exit')]
+              
+                ]
+    
+    _VARS['window'] = sg.Window('Calibration',
+                            layout,
+                            finalize=True,
+                            resizable=True,
+                            element_justification="right")
+
+    # Run the Event Loop
+    while True:
+        event, values = _VARS['window'].read(500)
+        if event in (sg.WIN_CLOSED, 'Quit', "Exit"):
+            break
+
+        if event == '-OPEN-':
+            filename = sg.popup_get_file("Select file to read", save_as=False, default_path=_VARS['window']["flash-data-filepath"].get())
+            #raw_data = window['-flash-data-']
+
+            if filename:
+                try:
+                    _VARS['window']["flash-data-filepath"].update(filename)
+                    # file = open(filename,'r')
+                    # lines= file.readlines()
+                    # file.close()
+                    ctable = pd.read_csv(filename)
+                except NotImplementedError:
+                    pass
+                fig = plt.figure()
+                #plt.plot(ctable["DAC1_2"],ctable["Voltage_Dec"], linewidth=1,color='r') #stampo la linea della retta interpolata
+                
+                X = ctable.DAC1_2
+                y = ctable.Voltage_Dec
+                X  = sm.add_constant(X)
+                model = sm.OLS(y,X).fit()
+                sg.Print(model.summary())
+                sg.Print(model.params)
+
+                plt.scatter(ctable["DAC1_2"],ctable["Voltage_Dec"],color='g') #ristampo i vfalori dei punti che ho misurato
+                y_predict = model.params[0] + model.params[1]*ctable['DAC1_2'] #metto dentro y_predict i valori con i coefficienti calcolati dal modello
+                plt.plot(ctable["DAC1_2"],y_predict, linewidth=1,color='r') #stampo la linea della retta interpolata
+
+                draw_figure(_VARS['window']['figCanvas'].TKCanvas, fig)
+
+
+if __name__ == '__main__':
+    sg.theme('DefaultNoMoreNagging') 
+    main()    
